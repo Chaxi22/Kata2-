6ea74c447dd1709f573ab8875c86947e97014c29
usuario <usuario@10.22.143.14>
2015-10-07 10:27:46 +0100

    Tercera Version

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -7,13 +7,11 @@ public class JavaApplication2 {
     public static void main(String[] args) {
        
         Integer[] vector = {1, 4, 7, 1, 1, 8, 4, 1, 4, 9};
-        
         String[] vector1 = {"Ana", "Juan", "Pedro", "Lucia", "Pedro", "Lucia"};
         
-        Histogram histo = new Histogram(vector1);
-        HashMap <Object, Integer> histogram = histo.getHisto();
+        Histogram <String> histogram = CalculaHistogram.computeHisto(vector1); 
         
-        for (Object key : histogram.keySet()) {
+        for (String key : histogram.keySet()) {
             System.out.println(key + " : " + histogram.get(key));
             
         }
